124bd98da00f915771a8e976c8cd6e6bc7cde33d
jeffreyally <jeffreyally@gmail.com>
2022-02-26 03:39:11 +0000

    in progress

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -32,11 +32,34 @@ const getState = ({ getStore, setStore, getActions }) => {
 			addToStoreContacts: newcontact => {
 				console.log(newcontact);
 				const store = getStore();
-				console.log(store.contacts);
+				const actions = getActions();
+				console.log("right here", actions);
 				let array3 = store.contacts.concat([newcontact]);
 				console.log(array3);
+				actions.postAContact();
 				setStore({ contacts: array3 });
 				return null;
+			},
+
+			postAContact: () => {
+				let sample = {
+					full_name: "Dave Bradley",
+					email: "GFSGBSFGRHGWRE$TRGRGRE@gmail.com",
+					agenda_slug: "IWantANiceCodeBeer",
+					address: "47568 NW 34ST, 33434 FL, USA",
+					phone: "7864445566"
+				};
+				fetch("https://assets.breatheco.de/apis/fake/contact/", {
+					method: "POST",
+					body: JSON.stringify(sample), // data can be `string` or {object}!
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(response => {
+						if (response.ok) console.log(response);
+					})
+					.catch(e => console.log(e, " THE ERROR"));
 			}
 		}
 	};
